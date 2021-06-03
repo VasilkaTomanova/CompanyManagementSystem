708094140674af63706c3098198c8072e95e0a42
Vasilka Tomanova <vasilkatomanova@gmail.com>
2021-06-03 15:01:50 +0300

    Create EmployeesProjects.cs

---
+++ CompanyManagementSystem/Data/Models/EmployeesProjects.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyManagementSystem.Data.Models
+{
+ public   class EmployeesProjects
+    {
+
+        public int EmployeeId { get; set; }
+
+        public Employee Employee { get; set; }
+
+
+        public int ProjectId { get; set; }
+
+        public Project Project { get; set; }
+
+    }
+}
