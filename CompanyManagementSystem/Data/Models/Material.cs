aff31e497ba4e98da7f6a366e776f4b280237903
Vasilka Tomanova <vasilkatomanova@gmail.com>
2021-06-03 14:14:02 +0300

    Create Material.cs

---
+++ CompanyManagementSystem/Data/Models/Material.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyManagementSystem.Data.Models
+{
+  public class Material
+    {
+        public int Id { get; set; }
+
+        public Employee Author { get; set; }
+
+        public int AuthorId { get; set; }
+
+
+
+
+
+
+    }
+}
