9d3e1fc5972e45653e6b8808c0bf86f4819288b5
Vasilka Tomanova <vasilkatomanova@gmail.com>
2021-06-03 13:38:11 +0300

    Create CompanyManagementSystemContext.cs

---
+++ CompanyManagementSystem/Data/CompanyManagementSystemContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CompanyManagementSystem.Data
+{
+   public class CompanyManagementSystemContext : DbContext
+    {
+        public CompanyManagementSystemContext()
+        {
+
+        }
+        public CompanyManagementSystemContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+
+
+
+
+
+    }
+}
