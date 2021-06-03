320ad45644aea53fee7a6e3bf9dd2cb0d4a94590
Vasilka Tomanova <vasilkatomanova@gmail.com>
2021-06-03 14:13:58 +0300

    Create Rank.cs

---
+++ CompanyManagementSystem/Data/Models/Rank.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyManagementSystem.Data.Models
+{
+ public   enum Range
+    {
+        Junior = 1,
+        Medium = 2,
+        Senior = 3
+    }
+}
