ac2f4db2b84207fa595d2e26b04ea2777f667423
Vasilka Tomanova <vasilkatomanova@gmail.com>
2021-06-03 16:48:43 +0300

    Update StartUp.cs

--- CompanyManagementSystem/StartUp.cs
+++ CompanyManagementSystem/StartUp.cs
@@ -1,5 +1,7 @@
 ﻿using CompanyManagementSystem.Data;
+using CompanyManagementSystem.Data.Models;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CompanyManagementSystem
@@ -9,20 +11,36 @@ namespace CompanyManagementSystem
         public static void Main(string[] args)
         {
 
-            var context = new CompanyManagementSystemContext();
+            CompanyManagementSystemContext context = new CompanyManagementSystemContext();
             context.Database.EnsureDeleted();
             context.Database.EnsureCreated();
 
 
-            //Console.Write("Enter your name:");
-            //string name = Console.ReadLine();
+            Console.Write("Enter your username:");
+            string name = Console.ReadLine();
 
-            //var empl = context.Employees.ToList();
-            //if (empl.Any(e=>e.Username == name))
-            //{
+            List<Employee> employyes = context.Employees.ToList();
+            Employee currentEmployee = employyes.FirstOrDefault(e => e.Username == name);
+            if (currentEmployee == null)
+            {
+                Console.WriteLine("Your username dosn't exist in our system. Sorry, try again later!");
+                return;
+            }
 
-            //}
-            //Console.Write("Enter your pass:");
+            Console.Write("Enter your pass:");
+            string pass = Console.ReadLine();
+
+            if (currentEmployee.Password != pass)
+            {
+                Console.WriteLine("Your username and password are diffrennt. Sorry, try again later!");
+                return;
+            }
+
+            while (true)
+            {
+                //read commmand
+            }
+
 
 
         }
