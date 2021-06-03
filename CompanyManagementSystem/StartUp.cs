0658fd8ec61cc60a57a3af2deda15bb3daf12934
Vasilka Tomanova <vasilkatomanova@gmail.com>
2021-06-03 13:38:01 +0300

    Create StartUp.cs

---
+++ CompanyManagementSystem/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using CompanyManagementSystem.Data;
+using System;
+
+namespace CompanyManagementSystem
+{
+  public  class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            var context = new CompanyManagementSystemContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+        }
+    }
+}
