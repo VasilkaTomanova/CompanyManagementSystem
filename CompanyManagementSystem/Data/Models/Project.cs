d16344f3e0987dd1cd5c07115d95fec62507689e
Vasilka Tomanova <vasilkatomanova@gmail.com>
2021-06-03 15:01:45 +0300

    Create Project.cs

---
+++ CompanyManagementSystem/Data/Models/Project.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyManagementSystem.Data.Models
+{
+public    class Project
+    {
+        public Project()
+        {
+            this.Employees = new HashSet<Employee>();
+        }
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Employee> Employees { get; set; }
+
+    }
+}
