45dd3b4892eb5c2f1a4bff332c4a0fe60ece6093
Vasilka Tomanova <vasilkatomanova@gmail.com>
2021-06-03 14:14:03 +0300

    Create Employee.cs

---
+++ CompanyManagementSystem/Data/Models/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyManagementSystem.Data.Models
+{
+    public class Employee
+    {
+        public Employee()
+        {
+
+        }
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public Position Position { get; set; }
+
+        public Rank Rank { get; set; }
+
+
+        public Employee Manager { get; set; }
+
+        public int ManagerId { get; set; }
+
+
+        public ICollection<Material> Materials { get; set; }
+
+    }
+}
