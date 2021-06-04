9e99191e35e37f120584dfa857453a056d0d1ceb
Vasilka Tomanova <vasilkatomanova@gmail.com>
2021-06-04 12:01:33 +0300

    Create Access.cs

---
+++ CompanyManagementSystem/Data/Models/Access.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyManagementSystem.Data.Models
+{
+  public  enum Access
+    {
+        Private = 1,
+        Public = 2,
+        Another = 3
+    }
+}
