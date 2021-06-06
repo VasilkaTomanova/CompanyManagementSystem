67e1ce07c2f91d51fa866a9b3ee89a8d291f74d4
Vasilka Tomanova <vasilkatomanova@gmail.com>
2021-06-06 14:13:22 +0300

    Update StartUp.cs

--- CompanyManagementSystem/StartUp.cs
+++ CompanyManagementSystem/StartUp.cs
@@ -10,10 +10,10 @@ namespace CompanyManagementSystem
         {
             //1.Create data base
             CompanyManagementSystemContext context = new CompanyManagementSystemContext();
-            //context.Database.EnsureDeleted();
-            //context.Database.EnsureCreated();
-            //CreateDataBase cb = new CreateDataBase();
-            //cb.CreateDatabase(context);
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            CreateDataBase cb = new CreateDataBase();
+            cb.CreateDatabase(context);
 
             //2. Entry in system 
             Engine startTheApp = new Engine(context);
