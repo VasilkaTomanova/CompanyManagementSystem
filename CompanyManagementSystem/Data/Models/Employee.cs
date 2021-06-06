2fc970c7f913d36ffc724bd88b6b07212b62d161
Vasilka Tomanova <vasilkatomanova@gmail.com>
2021-06-06 14:13:27 +0300

    Update Employee.cs

--- CompanyManagementSystem/Data/Models/Employee.cs
+++ CompanyManagementSystem/Data/Models/Employee.cs
@@ -3,43 +3,73 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CompanyManagementSystem.Data.Models
 {
     public class Employee
     {
+        //private const int minLenghtOfString = 3;
+        //private const int maxLength = 15;
+
+        private const string usernamePattern = @"[a-z]{3,15}";
+        private readonly Regex usernameRegex;
+
+        private const string passwordPattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{4,8}$";
+        private readonly Regex passowrdRegex;
+
+        private string username;
+        private string password;
+
+
         public Employee()
         {
             //Initialize the collection correct
             this.OwnMaterials = new HashSet<Material>();
             this.Projects = new HashSet<EmployeesProjects>();
+            this.usernameRegex = new Regex(usernamePattern);
+            this.passowrdRegex = new Regex(passwordPattern);
         }
         public int Id { get; set; }
 
         [Required]
-        //[MinLength(3)]
-        //[MaxLength(15)]
-        [RegularExpression(@"[A-Z][a-z]{2,15}")]
         public string FirstName { get; set; }
+        
 
         [Required]
-        //[MinLength(3)]
-        //[MaxLength(15)]
-        [RegularExpression(@"[A-Z][a-z]{2,15}")]
         public string LastName { get; set; }
 
         [Required]
-        [MinLength(3)]
-        [MaxLength(15)]
-        public string Username { get; set; }
+        //eventually with init but we need constructor
+        public string Username
+        {
+            get { return this.username; }
+            set
+            {
+                if (ValidateUsername(value) == false)
+                {
+                    throw new ArgumentException("Invalid format value of your username!");
+                }
+                this.username = value;
+            }
+        }
 
         [Required]
-        [MinLength(3)]
-        [MaxLength(15)]
-        public string Password { get; set; }
+        public string Password
+        {
+            get { return this.password; }
+            set
+            {
+                if (ValidatePassowrd(value) == false)
+                {
+                    throw new ArgumentException("Invalid format value of your passwrd!");
+                }
+                this.password = value;
+            }
+        }
 
-       
+
         public decimal? Salary { get; set; }
    
 
@@ -63,6 +93,30 @@ namespace CompanyManagementSystem.Data.Models
         {
             this.Salary = newSalary;
         }
+
+        private bool ValidateUsername (string stringToValidate)
+        {
+            Match match = this.usernameRegex.Match(stringToValidate);
+            if(match != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        //TODO
+        private bool ValidatePassowrd(string stringToValidate)
+        {
+            Match match = this.passowrdRegex.Match(stringToValidate);
+            if (match != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
 
     }
 }
